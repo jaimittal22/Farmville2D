e028b0570e2e8563b69694ac35a9fd08bd96a201
jaimittal <jai_mittal24@milton.edu>
2023-04-28 13:05:15 -0400

    committ 2

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -1,9 +1,49 @@
 public class MyFarm {
-    public static void main(String[] args) {
+    public static void main(String[] arge) {
         MyFarm charlie = new MyFarm();
+    }
+
+    public  Plot [][] grid;
+    public MyFarm() {
+        System.out.println("Hello from my farm!");
+        Plot Jai = new Plot();
+        Jai.printPlot();
+
+        grid = new Plot[4][5];
+
+        for (int n = 0; n < grid.length; n++) {
+            for (int r = 0; r < grid[n].length; r++) {
+                grid[n][r] = new Plot();
+                grid[n][r].printPlot();
+            }
+        }
+        totalPlants();
+        printPlantNames();
+    }
+
+    public void totalPlants() {
+        //how many total plants are there in the grid?
+        int sumOfPlants=0;
+
+        for(int e = 0; e<grid.length; e++){
+            for(int c=0; c<grid[e].length; c++){
+                System.out.println(grid[e][c].NumberOfPlants);
+            }
+
+        }
 
     }
-    public MyFarm(){
-        System.out.println("Hello from my farm!");
+    public void printPlantNames() {
+        // print the name of each plant in each row and column
+        // row: 0 col: 0 corn
+        for (int r = 0; r < grid.length; r++){
+            for(int c = 0; c <grid[r].length; c++ )
+            {
+                System.out.println("row:" + r +"col:" +c+ " " +grid[r][c].PlantName);
+            }
+
+
+        }
+
     }
 }
--- src/Plot.java
+++ src/Plot.java
@@ -3,16 +3,27 @@ public class Plot {
     public int NumberOfPlants;
     public boolean NeedsWater;
 
-    public Plot(){
-     //   PlantName = "basil";
-        NeedsWater = true;
-        NumberOfPlants = (int)(Math.random()*100)+10;
-        int num = (int)(Math.random()*5);
-        if (num==0){
+    public Plot() {
+        //   PlantName = "sunflower";
+        NeedsWater = false;
+        NumberOfPlants = (int) (Math.random() * 100) + 10;//random 10 110
+        int num = (int) (Math.random() * 5);
+        if (num == 0) {
             PlantName = "sunflower";
-        } else if (num==1) {
+        } else if (num == 1) {
             PlantName = "corn";
+        } else if (num == 2) {
+            PlantName = "carrot";
+        } else if (num == 3) {
+            PlantName = "tomato";
+        } else {
+            PlantName = "empty";
+            NumberOfPlants = 0;
 
         }
     }
+    public void printPlot(){
+        System.out.println("the plot has " + NumberOfPlants + " of " + PlantName + " and  ");
+    }
 }
+
